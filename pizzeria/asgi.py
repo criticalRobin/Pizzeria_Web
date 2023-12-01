8bd5f0002d55e920f827de2e4eb04973d50125f9
matite <matiastite1@gmail.com>
2023-12-01 15:31:20 -0500

    Commit inicial

---
+++ pizzeria/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for pizzeria project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": get_asgi_application(),
+#         "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+#     }
+# )
