944cbe08d4316863675b91261aee2e7d605cfded
matite <matiastite1@gmail.com>
2023-12-07 10:28:23 -0500

    Adición websockets comunicación tiempo real ordenes con cocina

--- pizzeria/asgi.py
+++ pizzeria/asgi.py
@@ -12,12 +12,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from pizzeria import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
 
-# application = ProtocolTypeRouter(
-#     {
-#         "http": get_asgi_application(),
-#         "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
-#     }
-# )
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+    }
+)
