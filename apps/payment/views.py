fa9e2e224f60969a42034aae5a1958d8dd820c9e
matite <matiastite1@gmail.com>
2024-01-11 14:33:43 -0500

    Agregada vista de pago exitoso

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from apps.api.models import Order
+from apps.api.models import Order, OrderDetails
 from apps.main.models import Client
 from .models import Payment
 from django.utils import timezone
@@ -19,8 +19,9 @@ class OrdersToPayListView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(OrdersToPayListView, self).get_context_data(**kwargs)
         orders_to_pay = Order.objects.filter(order_status="E")
+        orders_to_pay_details = OrderDetails.objects.filter(order__in=orders_to_pay)
         context["orders"] = orders_to_pay
-        print(context["orders"])
+        context["order_details"] = orders_to_pay_details
         return context
 
 
@@ -28,22 +29,24 @@ class OrdersToPayListView(LoginRequiredMixin, ListView):
 def create_order_payment(request, order_id):
     stripe.api_key = settings.STRIPE_API_KEY_HIDDEN
     order = Order.objects.get(id=order_id)
+    details = OrderDetails.objects.filter(order=order)
 
-    # Crear line_items con price_data basado en los detalles de la orden para Stripe
     line_items = [
         {
             "price_data": {
                 "currency": "usd",
                 "product_data": {
-                    "name": "Orden #" + str(order.id),
+                    "name": detail.product.name,
                 },
-                "unit_amount": int(order.total * 100),
+                "unit_amount": int(
+                    detail.product.sale_price * 100
+                ), 
             },
-            "quantity": 1,
+            "quantity": detail.quantity,
         }
+        for detail in details
     ]
 
-    # Crear la sesión de pago de Stripe
     checkout_session = stripe.checkout.Session.create(
         payment_method_types=["card"],
         line_items=line_items,
@@ -57,19 +60,16 @@ def create_order_payment(request, order_id):
         ),
     )
 
-    # Crear un registro de pago en la base de datos, si es necesario
     Payment.objects.create(
         order=order,
-        # Asegúrese de ajustar la siguiente línea para que refleje cómo se obtiene el cliente en su sistema
         client=Client.objects.get(id=1),
         amount=order.total,
         stripe_transaction_id=checkout_session["id"],
-        status="P",  # Pendiente
-        payment_method="T",  # Tarjeta
-        payment_date=timezone.now(),  # Importar timezone si aún no se ha hecho
+        status="P",
+        payment_method="T",
+        payment_date=timezone.now(),
     )
 
-    # Redirigir al usuario a la sesión de pago de Stripe
     return redirect(checkout_session.url, code=303)
 
 
@@ -86,7 +86,7 @@ def payment_success(request, order_id):
     order.save()
 
     # Redirigir al usuario a la página de éxito
-    return render(request, "payment/", {"order": order})
+    return render(request, "success.html", {"order": order})
 
 
 def payment_cancelled(request, order_id):
